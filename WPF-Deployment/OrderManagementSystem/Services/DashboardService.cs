a7f2f9d5d287a3ab7f2ccab4df79c6ec375b18f5
kedar-bhade <kedar.bhade@hotmail.com>
2025-12-02 06:37:51 +0000

    Added dashboard comment

--- WPF-Deployment/OrderManagementSystem/Services/DashboardService.cs
+++ WPF-Deployment/OrderManagementSystem/Services/DashboardService.cs
@@ -17,6 +17,7 @@ namespace OrderManagementSystem.Services
             _mapper = mapper;
         }
 
+        //Get dashboard data
         public async Task<DashboardDto> GetDashboardDataAsync()
         {
             try
